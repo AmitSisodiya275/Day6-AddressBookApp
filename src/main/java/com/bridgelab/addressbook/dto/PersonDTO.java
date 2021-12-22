4605596bf9f28b095a5cf3b616189a09930f2ee9
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-12-22 07:47:58 +0530

    Added: validation to the fields of the DTO class.

--- src/main/java/com/bridgelab/addressbook/dto/PersonDTO.java
+++ src/main/java/com/bridgelab/addressbook/dto/PersonDTO.java
@@ -1,14 +1,28 @@
 package com.bridgelab.addressbook.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class PersonDTO {
 
 	private int id;
+
+	@NotEmpty(message = "Name Cannot be Empty.")
+	@Pattern(regexp = "^[A-Z]{1}[A-Za-z]{2,}$", message = "Name is Invalid.")
 	private String name;
+
+	@Email(message = "Email is not in the proper format.")
 	private String email;
+
+	@NotEmpty(message = "City connot be Empty.")
 	private String city;
+
+	@NotEmpty
+	@Pattern(regexp = "^[0-9]{10}$", message = "Enter proper mobile number.")
 	private long mobileNo;
 
 }
